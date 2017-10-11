06bd7abfff1e3b6f59a3af853e626981d3a12792
Ria <h.sen.dev@gmail.com>
2017-10-11 18:57:29 +0330

    url match bugfix

--- src/ElasticCodeLensProvider.ts
+++ src/ElasticCodeLensProvider.ts
@@ -90,7 +90,8 @@ export class ElasticCodeLensProvider implements vscode.CodeLensProvider {
             vir = false
 
 
-            var regexp = /^(GET|POST|DELETE|PUT|OPTIONS|PATCH|HEAD)\s+([a-zA-z0-9_\/]+$)/gim;
+            // https://stackoverflow.com/a/1547940/1495442
+            var regexp = /^(GET|POST|DELETE|PUT|OPTIONS|PATCH|HEAD)\s+([A-Za-z0-9\-._~:\/#\[\]@!$&'\(\)\*+,;=`]+$)/gim;
             var match = regexp.exec(line.text);
 
             if (match != null) {
