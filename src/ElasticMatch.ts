e0bdee790d1075e701e369e89adc9a4e74620596
Ria <h.sen.dev@gmail.com>
2017-10-13 08:37:16 +0330

    better body range matcher

--- src/ElasticMatch.ts
+++ src/ElasticMatch.ts
@@ -28,16 +28,18 @@ export class ElasticMatch {
         let line = this.Method.Range.start.line + 1
         let ln = line
 
-        while (editor.document.lineCount > ln && editor.document.lineAt(ln).text.trim().length > 0) {
+        while (editor.document.lineCount > ln && editor.document.lineAt(ln).text.trim().length > 0) {            
             txt += editor.document.lineAt(ln).text + "\n"
-            lm = editor.document.lineAt(ln).text.length
+            lm = editor.document.lineAt(ln).text.length            
             ln++;
         }
+        
+        txt = txt.substring(0, txt.length - 1)
 
         let sp = new vscode.Position(line, 0)
-        let ep = new vscode.Position(ln, lm - 1)
+        let ep = new vscode.Position(ln - 1, lm)
         this.Body.Range = new vscode.Range(sp, ep)
-   
+
 
         try {
             JSON.parse(txt)
@@ -45,9 +47,9 @@ export class ElasticMatch {
             this.Body.Text = txt
         } catch (error) {
             console.error(error.message)
-            this.HasBody = false            
+            this.HasBody = false
             this.Error.Text = error.message
-            this.Error.Range =  this.GetErrorRangeFromMessage(txt, error.message)
+            this.Error.Range = this.GetErrorRangeFromMessage(txt, error.message)
         }
     }
 
