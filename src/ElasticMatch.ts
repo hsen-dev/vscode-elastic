133d7dd68d041e759eefacfde4815b7b3be8f893
Jozef Hartinger <jharting@redhat.com>
2019-08-02 10:20:06 +0200

    match URLs that are encoded (%22) or contain quotes (")

--- src/ElasticMatch.ts
+++ src/ElasticMatch.ts
@@ -11,7 +11,7 @@ export class ElasticItem {
 
 export class ElasticMatch {
 
-    static RegexMatch: RegExp = /^(GET|POST|DELETE|PUT)\s+([A-Za-z0-9\-\._~:\/#\[\]@!$&'\(\)\*+,;=`?]+)\s*$/gim;
+    static RegexMatch: RegExp = /^(GET|POST|DELETE|PUT)\s+([A-Za-z0-9\-\._~:\/#\[\]@!$&'"%\(\)\*+,;=`?]+)\s*$/gim;
     Error: ElasticItem
     Path: ElasticItem
     Body: ElasticItem
